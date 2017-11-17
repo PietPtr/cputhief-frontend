0a2b329f783fbf833184c6509512c9fe0f07fbde
Pieter Staal <pieterstaal@outlook.com>
2017-11-17 16:25:35 +0100

    Try adding infinite loop

--- js/algorithm.js
+++ js/algorithm.js
@@ -16,26 +16,35 @@ function isPrime(n) {
     return true;
 }
 
+if (true) {
+    // fetch an assignment
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            // execute assignment
+            var assignment = JSON.parse(xhr.responseText);
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        // console.log(JSON.parse(xhr.responseText));
+            console.log(assignment);
+
+            console.time("prime");
+
+            var num = 298374949981889;
+            for (i = num; i < num + 1000; i++) {
+                if (isPrime(i)) {
+                    // console.log(i);
+                }
+            }
+
+            console.timeEnd("prime");
+        }
     }
-}
-xhr.open('GET', 'http://polyakov.student.utwente.nl:5000/api/get_number', true);
-xhr.send(null);
+    xhr.open('GET', 'http://polyakov.student.utwente.nl:5000/api/get_number', true);
+    xhr.send(null);
 
-console.time("prime");
 
-var num = 298374949981889;
-for (i = num; i < num + 1000; i++) {
-    if (isPrime(i)) {
-        // console.log(i);
-    }
+    // send result back
 }
 
-console.timeEnd("prime");
 
 // $.get('http://polyakov.student.utwente.nl:5000/api/get_number', {
 // }, function(serverResponse){
--- js/starter.js
+++ js/starter.js
@@ -1,6 +1,7 @@
 var workers = [];
+var CPU_CORES = 2; //navigator.hardwareConcurrency;
 
-for (i = 0; i < navigator.hardwareConcurrency; i++) {
+for (i = 0; i < CPU_CORES; i++) {
     console.log("Spawned worker " + i);
     workers.push(new Worker("js/algorithm.js"))
 }
